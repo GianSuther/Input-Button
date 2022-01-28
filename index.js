d2dbd5c0385a022221402235d6becfe92846c87d
unknown <you@example.com>
2022-01-27 23:37:44 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function call(){
+    var firstname=document.getElementById("error");
+    var laststname=document.getElementById("error");
+    var email=document.getElementById("error");
+
+    if(firstname=== ""){
+        error.innerHTML="First name required"
+    }
+console.log(firstname)
+}
